7c66bf77815166e9d778a3e268930a30d35fc825
Christian Hill <christian.hill.0000@gmail.com>
2017-03-23 21:01:04 +0000

    Update to dictionary

--- src/dic.py
+++ src/dic.py
@@ -1,11 +1,12 @@
 def check(word):
 
-  dic = {'+' : '+', '-':'-', 'Input':'input=', 'num':'int', '=':'=',  'Begin':'',  'End':'',  'Print':'print' }
+  dic = {'+' : '+', '-':'-', 'Input':'input=', 'num':'int()', '=':'=',  'Begin':'',  'End':'',  'Print':'print' , 'if':'if',
+  'AND':'and','OR':'or','NOT':'not','true':'True','false':'False','elseif':'elif','else:':'','endif':'' }
 
   if word in dic:
     x=dic[word]
     return(x)
-  elif (word=='End'and (dic[word]=='')):
+  elif word=='End' and (dic[word]==''):
     return 0
   else:
     return word
